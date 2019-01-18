db28101b0c6dad118259e5b2217f3129c8de6f47
Alex <keeley.lp@gmail.com>
2019-01-18 10:42:39 +0000

    heroku removed jsonwebtoken

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-const express = require('express');
-const body_parser = require('body-parser');
-const { handle404, handle400 } = require('./errors');
-const { sendToken, verifyToken } = require('./auth');
+const express = require("express");
+const body_parser = require("body-parser");
+const { handle404, handle400 } = require("./errors");
 
 const app = express();
 
-const apiRouter = require('./routes/apiRouter');
+const apiRouter = require("./routes/apiRouter");
 
 app.use(body_parser.json());
 
-// app.post("/login", sendToken);
-
-// app.use(verifyToken);
-
-app.get('/', (req, res) => {
-  res.send('api');
+app.get("/", (req, res) => {
+  res.send("api");
 });
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 app.use(handle400);
 app.use(handle404);
